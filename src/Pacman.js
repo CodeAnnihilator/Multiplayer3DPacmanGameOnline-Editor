0e13d12e48753a360b885df1bad7634edd9822e8
Code Annihilator <frostberryart@gmail.com>
2017-11-21 19:36:27 +0300

    pacman movement improvement

--- src/Pacman.js
+++ src/Pacman.js
@@ -10,8 +10,11 @@ export default class Pacman extends Component {
     this.initialPosition = level1.reduce((c, n, y) => (n.reduce((c1, n1, x) => n1.groundType === 5 ? c = { x, y } : ''), c), {})
     this.state = {
       isMoving: false,
-      dX: 0,
-      dY: 0,
+      speed: 0.01,
+      diffX: 0,
+      diffY: 0,
+      nextX: this.initialPosition.x,
+      nextY: this.initialPosition.y,
       x: this.initialPosition.x,
       y: this.initialPosition.y,
       z: 0,
@@ -20,43 +23,67 @@ export default class Pacman extends Component {
 
   componentDidMount() {
     document.addEventListener("keydown", this.handleKeyPress, false)
-    this.countdown = setInterval(this.timer, 5)
+    document.addEventListener("keyup", this.handleKeyUp, false)
+    this.countdown = setInterval(this.move, 5)
   }
 
   componentWillUnmount() {
     document.removeEventListener("keydown", this.handleKeyPress, false)
+    document.removeEventListener("keyup", this.handleKeyUp, false)
+    clearInterval(this.countdown)
   }
 
-  timer = () => {
-    if (this.state.isMoving) {
-      if (this.state.x === this.state.dX && this.state.y === this.state.dY) {
-        return this.setState({ isMoving: false })
-      } else {
-        return this.setState({
-          x: this.state.x + (this.state.dX - this.state.x) * 0.1,
-          y: this.state.y + (this.state.dY - this.state.y) * 0.1
-        })
-      }
+  isNextTileReached = () => {
+    const { x, y, nextX, nextY, diffX, diffY } = this.state
+    const dirX = diffX > 0 ? 1 : 0
+    const dirY = diffY > 0 ? 1 : 0
+    const isReachedX = dirX ? x >= nextX : x <= nextX
+    const isReachedY = dirY ? y >= nextY : y <= nextY
+    return isReachedX && isReachedY
+  }
+
+  move = () => {
+    const { speed, x, y, diffX, diffY, nextX, nextY, isMoving } = this.state
+    const nextXpos = x + diffX * speed
+    const nextYpos = y + diffY * speed
+    const isNextTileReached = this.isNextTileReached()
+    if (isMoving && isNextTileReached) {
+      const nextDestinationTile = level1[nextY + diffY][nextX + diffX]
+      if (nextDestinationTile.groundType === 0) return this.setState({ x: nextXpos, y: nextYpos, isMoving: false })
+    }
+    if (!isMoving) {
+      if (!isNextTileReached) return this.setState({ x: nextXpos, y: nextYpos })
+      if (isNextTileReached) return this.setState({ x: nextX, y: nextY })
+    } else {
+      if (!isNextTileReached) this.setState({ x: nextXpos, y: nextYpos })
+      if (isNextTileReached) this.setState({ x: nextXpos, y: nextYpos, nextX: nextX + diffX, nextY: nextY + diffY })
     }
   }
 
-  setNewPosition = (x, y) => {
-    if (this.state.isMoving) return
-    const destinationTile = level1[y][x]
+  checkDestination = (nextX, nextY) => {
+    const destinationTile = level1[nextY][nextX]
     if (destinationTile.groundType === 0) return
-    this.setState({ dX: x, dY: y, isMoving: true })
+    const diffX = nextX - this.state.nextX
+    const diffY = nextY - this.state.nextY
+    this.setState({ diffX, diffY, nextX, nextY, isMoving: true })
   }
 
   handleKeyPress = e => {
-    if (e.keyCode === 37) this.setNewPosition(this.state.x - 1, this.state.y)
-    if (e.keyCode === 39) this.setNewPosition(this.state.x + 1, this.state.y)
-    if (e.keyCode === 38) this.setNewPosition(this.state.x, this.state.y + 1)
-    if (e.keyCode === 40) this.setNewPosition(this.state.x, this.state.y - 1)
+    if (this.state.isMoving) return
+    // const isNextTileReached = this.isNextTileReached()
+    // TODO: disable move y if x is moving
+    if (e.keyCode === 37) this.checkDestination(this.state.nextX - 1, this.state.nextY)
+    if (e.keyCode === 39) this.checkDestination(this.state.nextX + 1, this.state.nextY)
+    if (e.keyCode === 38) this.checkDestination(this.state.nextX, this.state.nextY + 1)
+    if (e.keyCode === 40) this.checkDestination(this.state.nextX, this.state.nextY - 1)
+  }
+
+  handleKeyUp = e => {
+    this.setState({ isMoving: false })
   }
 
   render() {
     const position = new Vector3(this.state.x, this.state.y, 0)
-    console.log(this.state.x)
     return (
       <group>
         <Camera cameraPositionX={this.state.x} cameraPositionY={this.state.y} />
@@ -68,3 +95,53 @@ export default class Pacman extends Component {
     )
   }
 }
+
+// constructor() {
+//   super()
+//   this.initialPosition = level1.reduce((c, n, y) => (n.reduce((c1, n1, x) => n1.groundType === 5 ? c = { x, y } : ''), c), {})
+//   this.state = {
+//     isMoving: false,
+//     dX: 0,
+//     dY: 0,
+//     x: this.initialPosition.x,
+//     y: this.initialPosition.y,
+//     z: 0,
+//   }
+// }
+
+// timer = () => {
+//   if (this.state.isMoving) {
+//     const isReachedDX = parseFloat((this.state.dX - this.state.x).toFixed(10)) === 0
+//     const isReachedDY = parseFloat((this.state.dY - this.state.y).toFixed(10)) === 0
+//     if (isReachedDX && isReachedDY) {
+//       console.log(this.state.x)
+//       console.log(this.state.dX)
+//       return this.setState({
+//         isMoving: false,
+//         x: this.state.dX,
+//         y: this.state.dY
+//       })
+//     } else {
+//       const x1 = this.state.dX - this.state.x
+//       const y1 = this.state.dY - this.state.y
+//       return this.setState({
+//         x: !isReachedDX ? x1 > 0 ? this.state.x + 0.005 : this.state.x - 0.005 : this.state.dX,
+//         y: !isReachedDY ? y1 > 0 ? this.state.y + 0.005 : this.state.y - 0.005 : this.state.dY
+//       })
+//     }
+//   }
+// }
+//
+// setNewPosition = (x, y) => {
+//   if (this.state.isMoving) return
+//   const destinationTile = level1[y][x]
+//   if (destinationTile.groundType === 0) return
+//   this.setState({ dX: x, dY: y, isMoving: true })
+// }
+
+// handleKeyPress = e => {
+//   if (e.keyCode === 37) this.setNewPosition(this.state.x - 1, this.state.y)
+//   if (e.keyCode === 39) this.setNewPosition(this.state.x + 1, this.state.y)
+//   if (e.keyCode === 38) this.setNewPosition(this.state.x, this.state.y + 1)
+//   if (e.keyCode === 40) this.setNewPosition(this.state.x, this.state.y - 1)
+// }
